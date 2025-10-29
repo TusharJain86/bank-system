6486fc871b54b8b748d8db278b3410cc496df661
TusharJain86 <tusharjain0806@gmail.com>
2025-10-29 18:41:25 +0530

    Initial commit for bank system

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to our bank!");
+        System.out.println("""
+            Please choose an option:
+            1. Create Account
+            2. Deposit Money
+            3. Withdraw Money
+            4. Transfer Money
+            5. Account Statement
+            6. List All Accounts
+            7. Search Account by Holder's Name
+            0. Exit
+        """);
+    }
+}
