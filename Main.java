9c6ba43b473fa967053374b1ce11b325789f27f5
TusharJain86 <tusharjain0806@gmail.com>
2025-10-29 19:09:59 +0530

    Added new files regarding user and updated main class

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, Welcome to our bank!");
-        System.out.println("""
-            Please choose an option:
-            1. Create Account
-            2. Deposit Money
-            3. Withdraw Money
-            4. Transfer Money
-            5. Account Statement
-            6. List All Accounts
-            7. Search Account by Holder's Name
-            0. Exit
-        """);
+
+        Boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("""
+                        1. Create Account
+                        2. Deposit Money
+                        3. Withdraw Money
+                        4. Transfer Money
+                        5. Account Statement
+                        6. List All Accounts
+                        7. Search Account by Holder's Name
+                        0. Exit
+                    """);
+            System.out.print("Please select an option: ");
+            String choice = scanner.nextLine().trim();
+            System.out.println("You selected option: " + choice);
+
+            switch (choice){
+                case "0" -> isRunning = false;
+            }
+        }
     }
 }
